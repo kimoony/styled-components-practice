c990dbb6d65e64084f723ff91bbde3603a97e138
kimoony <hoonby9011@gmail.com>
2022-03-16 21:59:40 +0900

    :memo: update Animation.js
    pseudo selector로 컴포넌트 안에 컴포넌트 선택해 이벤트 적용하기

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -21,26 +21,29 @@ const rotationAnimation = keyframes`
   }
 `
 
-const Box = styled.div`
-height: 200px;
-width: 200px;
-background-color: red;
-animation: ${rotationAnimation} 1s linear infinite;
-display: flex;
-justify-content: center;
-align-items: center;
-/* component 안에서 element 선택 */
-span {
-  font-size: 40px;
+const Emoji = styled.span`
+  font-size: 38px;
   transition: 1s all;
-  &:hover { /* span:hover{} 와 같다. */
-    font-size: 100px;
-    transition: 1s all;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: red;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* pseudo selector */
+  /* Box component 안에서 element 선택 */
+  /* span{ */
+  /* Box component 안에 Emoji component 선택 */
+  ${Emoji} {
+    &:hover { /* span:hover{} 와 같다. */
+      font-size: 100px;
+      transition: 1s all;
+    }
   }
-  &:active {
-    opacity: 0;
-  }
-}
 `;
 
 const Animation = () => {
@@ -48,8 +51,12 @@ const Animation = () => {
     <Wrapper>
       <Box>
         {/* component 안에 element 도 사용할 수 있다. */}
-        <span>🤪</span>
+        {/* <span>🤪</span> */}
+        <Emoji>🤪</Emoji>
       </Box>
+      {/* hover 적용되지 않음
+      이유는 - hover 이벤트는  Box component 안에 있는 Emoji component에만 적용됨 */}
+      <Emoji>🤯</Emoji>
     </Wrapper>
   )
 }
